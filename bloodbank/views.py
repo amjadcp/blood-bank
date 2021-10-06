0ab50b7afe14be4cf8793c91d0533a673b280e38
amjadcp <amjadcp03@gmail.com>
2021-10-06 10:09:37 +0530

    d16

--- bloodbank/views.py
+++ bloodbank/views.py
@@ -1,20 +1,23 @@
 from django.contrib import auth
-from django.http import request
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib import messages
 from .models import *
 from django.http import JsonResponse
+from django.contrib import sessions
 
 # Create your views here.
 
 def login(request):
+    if request.session.has_key('username'):
+        return redirect('table')
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
         if user is not None:
+            request.session['username'] = username
             data = Add_member.objects.all()
             return render(request, 'table.html', {'username' : username, 'data' : data})
         
@@ -27,6 +30,8 @@ def login(request):
       return render(request, 'login.html')
 
 def signup(request):
+    if request.session.has_key('username'):
+        return redirect('table')
     if request.method == 'POST':
        username = request.POST['username']
        password1 = request.POST['password1']
@@ -52,29 +57,41 @@ def signup(request):
 
 def table(request):
     data = Add_member.objects.all()
-    return render(request, 'table.html', {'data' : data})
+    if request.session.has_key('username'):
+       username =  request.session['username']
+       return render(request, 'table.html', {'data' : data, 'username' : username})
+    else:
+        return redirect('login')
+
 
 def add_member(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        age = int(request.POST['age'])
-        phone_number = int(request.POST['phonenumber'])
-        blood_group = request.POST['bloodgroup']
-        Add_member.objects.create(
-            name=name,
-            age=age,
-            phone_number=phone_number,
-            blood_group=blood_group
-        )
-        data = Add_member.objects.all()
-        for element in data:
-           print(element.id)
-        return redirect('table')
+    # username = request.session['username']
+    if request.session.has_key('username'):
+       if request.method == 'POST':
+            name = request.POST.get['name']
+            age = int(request.POST.get['age'])
+            phone_number = int(request.POST.get['phonenumber'])
+            blood_group = request.POST.get['bloodgroup']
+            Add_member.objects.create(
+                name=name,
+                age=age,
+                phone_number=phone_number,
+                blood_group=blood_group
+            )
+            data = Add_member.objects.all()
+            for element in data:
+             print(element.id)
+            return redirect('table')
+       else:
+           return render(request, 'add_member.html')
+
     else:
-       return render(request, 'add_member.html')
+        return redirect('login')
+         
 
 def logout(request):
-    auth.logout(request)
+    del request.session['username']
+    # auth.logout(request)
     return redirect('login')
 
 def check(request):
